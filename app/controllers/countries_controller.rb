bbed035e93e9f11722cd58c8c07711fb945e8527
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-07-28 05:15:57 +0900

    delete

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,25 +1,5 @@
 class CountriesController < ApplicationController
   def index
-    # @countries = Country.all
-    # @hash_id_name = Country.pluck(:id, :name).to_h
-    # Country.all.map do |country|
-    #   if country.name.length >= 3 
-    #     country.name.length
-    #   end
-    # end
-    # @map_countries = Country.all.map{|country| country.name.length }
-    # Country.all.map{&:name.length }
-    # @each_countries = []
-    # Country.all.each do |country|
-    #   @each_countries << country.name.length
-    # end
-    # capital_food_hash = Country.pluck(:capital, :food).to_h
-    # capital_food_hash.map do  |k, v|
-    #   if k.length >= 3
-    #     [ k.length, v]
-    #   end 
-    # end
-    sports = Country.pluck(:sports)
-    sports.map{|s| s.length}
+  
   end
 end
